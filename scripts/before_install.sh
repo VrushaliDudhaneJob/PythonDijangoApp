e567e5ccaaa3f654c3c979002243d845a251609e
VrushaliDudhaneJob <vrushalidudhane.jobs@gmail.com>
2026-07-07 21:06:18 +0530

    added more

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 # clean codedeploy-agent files for a fresh install
-sudo rm -rf /home/ubuntu/install
+#sudo rm -rf /home/ubuntu/install
 
 # install CodeDeploy agent
-sudo apt-get -y update
-sudo apt-get -y install ruby
-sudo apt-get -y install wget
-cd /home/ubuntu
-wget https://aws-codedeploy-ap-south-1.s3.ap-south-1.amazonaws.com/latest/install
-sudo chmod +x ./install 
-sudo ./install auto
-systemctl start codedeploy-agent
+# sudo apt-get -y update
+# sudo apt-get -y install ruby
+# sudo apt-get -y install wget
+# cd /home/ubuntu
+# wget https://aws-codedeploy-ap-south-1.s3.ap-south-1.amazonaws.com/latest/install
+# sudo chmod +x ./install 
+# sudo ./install auto
+# systemctl start codedeploy-agent
 
 
 # update os & install python3
